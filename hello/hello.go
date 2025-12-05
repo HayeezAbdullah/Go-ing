4211a774d3f9efb67ba3685691f2f0560db7bd82
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-12-05 23:07:20 +0530

    done 2 pi r comparison program

--- hello/hello.go
+++ hello/hello.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	const Pi = 3.14159
 	var r int = 34
+	var another_r int = 66
 
-	fmt.Println("Circumference: ", 2 * Pi * float64(r))
+	fmt.Println("r is bigger than another_r ? -> ", (2 * Pi * float64(r)) > (2 * Pi * float64(another_r)))
 
 }
