631c0576ea7ba20d1d346d73cc0b2fb40f076c54
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-12-05 23:01:24 +0530

    float64

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var age int = 20
-	var name string = "Dayvid"
-	var isStudent bool = true
-
-
-	fmt.Println("Name:", name)
-	fmt.Println("age", age)
-	fmt.Println("Is Student?", isStudent)
+	var score float64 = 4.3
+	fmt.Println(score)
 
 }
