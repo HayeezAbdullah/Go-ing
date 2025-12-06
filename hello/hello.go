3d096315448f57f63e05ac8b80497c0f9ab3dead
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-12-06 13:46:52 +0530

    seperated logic and input output functions

--- hello/hello.go
+++ hello/hello.go
@@ -1,31 +1,40 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	var num1 float64
-	var num2 float64
-	var operator string
-
-	fmt.Println("Enter the first number: ")
-	fmt.Scanln(&num1)
-	fmt.Println("Enter the second number: ")
-	fmt.Scanln(&num2)
-	fmt.Println("Enter Operator (+,-,*,/): ")
-	fmt.Scanln(&operator)
-	
+func calculate(num1, num2 float64, operator string) (float64, error) {
 	switch operator {
 	case "+":
-		fmt.Printf(`Result %v`, num1 + num2)
+		return num1 + num2, nil
 	case "-":
-		fmt.Printf(`Result %v`, num1 - num2)
+		return num1 - num2, nil
 	case "*":
-		fmt.Printf(`Result %v`, num1 * num2)
+		return num1 * num2, nil
 	case "/":
-		fmt.Printf(`Result %v`, num1 / num2)
+		if num2 == 0 {
+			return 0, fmt.Errorf("cannot divide by zero")
+		}
+		return num1 / num2 , nil
 	default:
-		fmt.Println("Invalid Operator")	
+		return 0, fmt.Errorf("invalid input")
 	}
+}
+
+func main() {
+	var input1 , input2 float64
+	var operator_string string
+
+	fmt.Println("Enter first number: ")
+	fmt.Scanln(&input1)
+	fmt.Println("Enter operator: ")
+	fmt.Scanln(&operator_string)
+	fmt.Println("Enter second number: ")
+	fmt.Scanln(&input2)
+
+	result, err := calculate(input1, input2, operator_string)
+	if err != nil {
+		fmt.Println("Caught error: ", err)
+		return
+	}
+	fmt.Printf(`Result is %v`, result)
 }
