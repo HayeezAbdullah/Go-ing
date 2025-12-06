7bc52c8d91cd4fdd3a20986ba2a465458d5e890c
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-12-06 13:20:31 +0530

    calculator lesson 1

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	var num1 float64
+	var num2 float64
+	var operator string
+
+	fmt.Println("Enter the first number: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter the second number: ")
+	fmt.Scanln(&num2)
+	fmt.Println("Enter Operator (+,-,*,/): ")
+	fmt.Scanln(&operator)
 	
-	var person_two_like_tea bool = false
-
-	fmt.Println(!person_two_like_tea)
-
+	switch operator {
+	case "+":
+		fmt.Printf(`Result %v`, num1 + num2)
+	case "-":
+		fmt.Printf(`Result %v`, num1 - num2)
+	case "*":
+		fmt.Printf(`Result %v`, num1 * num2)
+	case "/":
+		fmt.Printf(`Result %v`, num1 / num2)
+	default:
+		fmt.Println("Invalid Operator")	
+	}
 }
