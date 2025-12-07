bb795e7111e2a8cd9478afeec655d838b2b9c549
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-12-07 13:04:30 +0530

    adult /minor program

--- ifelse/ifelse.go
+++ ifelse/ifelse.go
@@ -2,30 +2,11 @@ package main
 
 import "fmt"
 
-func get_quantity(quantity int, availableStock int) ( string, error){
-	if quantity <= 0 {
-		return "Quantity must be positive", nil
-	}
-	if quantity > availableStock{
-		return "Quantity not enough", nil
-	}
-
-	return "Order accepted", nil
-}
-
 func main() {
-	var customerReq, inStock int
-
-	fmt.Println("Enter customer requirement: ")
-	fmt.Scanln(&customerReq)
-
-	fmt.Println("Enter Instock: ")
-	fmt.Scanln(&inStock)
-
-	result, err := get_quantity(customerReq, inStock)
-	if err != nil {
-		fmt.Println("error instock", err)
-		return
+	var age int = 12
+	if age >= 18 {
+		fmt.Println("Adult")
+	} else {
+		fmt.Println("Minor")
 	}
-	fmt.Println(result)
 }
