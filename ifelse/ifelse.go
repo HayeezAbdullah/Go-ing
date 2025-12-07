fa443173eef1b04780194a00bbd2a96fe39ec3a9
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2025-12-07 13:10:43 +0530

    switch case

--- ifelse/ifelse.go
+++ ifelse/ifelse.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var age int = 12
-	if age >= 18 {
-		fmt.Println("Adult")
-	} else {
+	var age int
+	fmt.Println("Enter age: ")
+	fmt.Scanln(&age)
+
+	switch {
+	case age < 18:
 		fmt.Println("Minor")
+	case (age > 17) && (age < 59):
+		fmt.Println("Adult")
+	default:
+		fmt.Println("Old")	
 	}
+
 }
